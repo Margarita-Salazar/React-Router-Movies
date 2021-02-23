badcb137d06d70dbea92b3c0ba8977aa5ae1b000
unknown <margarita.salazar.mls@gmail.com>
2021-02-23 01:59:40 -0600

    added router on app component, and added route that will take an id parameter

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -14,6 +16,8 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          console.log(response.data)
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +34,16 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <MovieList movies={movieList}/>
+          </Route>
+          <Route path='/movies/:id'>
+            <Movie/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
